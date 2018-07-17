416de971cd4a6091fc3db4739e41111bce0c5816
shaoding <qintianc@126.com>
2018-07-17 15:36:07 +0800

    first commit

---
+++ src/main/java/com/example/mudiProj/MudiProjApplication.java
@@ -0,0 +1,12 @@
+package com.example.mudiProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiProjApplication.class, args);
+	}
+}
