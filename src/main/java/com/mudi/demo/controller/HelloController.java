362886366806deb4dfc2600b5d216c3601017d57
shaoding <qintianc@126.com>
2018-07-24 14:54:54 +0800

    添加mybatis-demo

--- src/main/java/com/mudi/demo/controller/HelloController.java
+++ src/main/java/com/mudi/demo/controller/HelloController.java
@@ -1,16 +1,31 @@
 package com.mudi.demo.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mudi.demo.data.DataTest;
 
 @Controller
 @RequestMapping("/demo")
 public class HelloController {
 
+	@Autowired
+	private DataTest dataTest;
+
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String view() {
 		return "hello world";
+	}
+
+	@RequestMapping("/mybatis")
+	@ResponseBody
+	public Map<String, Object> mybatis() {
+		Map<String, Object> info = dataTest.getInfo();
+		return info;
 	}
 }
---
+++ src/main/java/com/mudi/demo/data/DataTest.java
@@ -0,0 +1,13 @@
+package com.mudi.demo.data;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface DataTest {
+
+	@Select("select NAME,AGE from T_NAME WHERE ID = 1")
+	public Map<String,Object> getInfo();
+}
